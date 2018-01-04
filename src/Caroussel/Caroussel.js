c7c07e6e15199b128a3bbad9839151d8d61989b1
Marcelo Motta Bryan <dark7yuki@gmail.com>
2018-01-04 22:59:37 +0100

    add caroussel example

---
+++ src/Caroussel/Caroussel.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Caroussel extends Component {
+  render() {
+    return (
+      <span> Caroussel </span>
+    );
+  }
+}
+
+export default Caroussel;
