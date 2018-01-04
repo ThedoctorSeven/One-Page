cf8a223674c04f03b19b7f6c9cbc7e987a5577d0
Riouallon Vincent <riouallonvincent@gmail.com>
2018-01-04 22:11:29 +0100

    base

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Button from 'material-ui/Button'
+
+class App extends Component {
+  render() {
+    return (
+      <Button raised color='primary'>
+        yolo bite
+      </Button>
+    );
+  }
+}
+
+export default App;
