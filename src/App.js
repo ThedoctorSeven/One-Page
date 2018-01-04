1fb85469b4c54631035f0d4703bffdeeb0fd2598
Marcelo Motta Bryan <dark7yuki@gmail.com>
2018-01-04 22:59:27 +0100

    change architecture app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
-import Button from 'material-ui/Button'
+import {Button, AppBar, Toolbar, Typography} from 'material-ui'
+import Caroussel from './Caroussel/Caroussel.js'
+
 
 class App extends Component {
   render() {
     return (
-      <Button raised color='primary'>
-        yolo bite
-      </Button>
+
+      // Menu
+      //joke => Ajax   (Champs texte, bouton refresh)
+      //Caroussel (image => ajax)
+      
+      <div>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Typography type="title" color="inherit">
+              Ma couille
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Button raised color='primary'>
+          yolo bite
+        </Button>
+        <Caroussel/>
+      </div>
+      //footer
     );
   }
 }
