af5539f1e76f4b5848ba12670a74ad053d48c197
Riouallon Vincent <riouallonvincent@gmail.com>
2018-01-08 13:55:31 +0100

    Indentation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
-import {AppBar, Toolbar, Typography, Grid} from 'material-ui'
-import {CarouselContainer, Form, Map} from './Common'
+import { AppBar, Toolbar, Typography, Grid } from 'material-ui'
+import { CarouselContainer, Form, Map } from './Common'
 
 
 
@@ -14,33 +14,33 @@ class App extends Component {
       //joke => Ajax   (Champs texte, bouton refresh)
       //Caroussel (image => ajax)
       <div >
-			<MuiThemeProvider theme={theme}>
+        <MuiThemeProvider theme={theme}>
 
-        <AppBar position="static" color="default">
-          <Toolbar>
-            <Typography type="title" color="inherit">
-              One Page
+          <AppBar position="static" color="default">
+            <Toolbar>
+              <Typography type="title" color="inherit">
+                One Page
             </Typography>
-          </Toolbar>
-        </AppBar>
+            </Toolbar>
+          </AppBar>
 
-        <Grid container spacing={40} alignItems="center" direction="row" justify="center">
-          <Grid item xs={12} sm={10}>
-            <CarouselContainer/>
+          <Grid container spacing={40} alignItems="center" direction="row" justify="center">
+            <Grid item xs={12} sm={10}>
+              <CarouselContainer />
+            </Grid>
+            <Grid item xs={12} sm={10}>
+              <Form />
+            </Grid>
+            <Grid item xs={12}>
+              <Map />
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={10}>
-            <Form/>
-          </Grid>
-          <Grid item xs={12}>
-            <Map/>
-          </Grid>
-        </Grid>
 
 
-      </MuiThemeProvider>
+        </MuiThemeProvider>
 
       </div>
-      
+
     );
   }
 }
