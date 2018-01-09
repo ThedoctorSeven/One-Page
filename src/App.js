545d4701fc3fb42b4477ec4e5d4ad48aaa30e446
Marcelo Motta Bryan <dark7yuki@gmail.com>
2018-01-09 19:11:58 +0100

    smooth scroll et

--- src/App.js
+++ src/App.js
@@ -1,46 +1,53 @@
-import React, { Component } from 'react';
-import './App.css';
-import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
-import { AppBar, Toolbar, Typography, Grid } from 'material-ui'
-import { CarouselContainer, Form, Map } from './Common'
-
-
+import React, { Component } from "react";
+import "./App.css";
+import { MuiThemeProvider, createMuiTheme } from "material-ui/styles";
+import { AppBar, Toolbar, Typography, Grid, Button } from "material-ui";
+import { CarouselContainer, Form, Map } from "./Common";
+import smoothscroll from "smoothscroll";
 
 class App extends Component {
+  scroll = id => {
+    smoothscroll(document.getElementById(id));
+  };
+
   render() {
     const theme = createMuiTheme();
     return (
       // Menu
       //joke => Ajax   (Champs texte, bouton refresh)
       //Caroussel (image => ajax)
-      <div >
+      <div>
         <MuiThemeProvider theme={theme}>
-
           <AppBar position="static" color="default">
             <Toolbar>
               <Typography type="title" color="inherit">
                 One Page
-            </Typography>
+              </Typography>
+              <Button onClick={() => this.scroll("map")} raised color="primary">
+                Ajouter une photo
+              </Button>
             </Toolbar>
           </AppBar>
 
-          <Grid container spacing={40} alignItems="center" direction="row" justify="center">
+          <Grid
+            container
+            spacing={40}
+            alignItems="center"
+            direction="row"
+            justify="center"
+          >
             <Grid item xs={12} sm={10}>
               <CarouselContainer />
             </Grid>
             <Grid item xs={12} sm={10}>
               <Form />
             </Grid>
-            <Grid item xs={12}>
+            <Grid item xs={12} id="map">
               <Map />
             </Grid>
           </Grid>
-
-
         </MuiThemeProvider>
-
       </div>
-
     );
   }
 }
--- src/Common/Form/Form.js
+++ src/Common/Form/Form.js
@@ -1,128 +1,138 @@
-import React, { Component } from 'react';
-import {Button, Card, CardActions, CardContent, CardHeader, TextField, withStyles, Icon } from 'material-ui'
-import { DatePicker } from 'material-ui-pickers';
-import moment from 'moment';
-import PropTypes from 'prop-types';
-
-
+import React, { Component } from "react";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  TextField,
+  withStyles,
+  Icon
+} from "material-ui";
+import { DatePicker } from "material-ui-pickers";
+import moment from "moment";
+import PropTypes from "prop-types";
 
 const styles = theme => ({
-    container: {
-      display: 'flex',
-      flexWrap: 'wrap',
-    },
-    textField: {
-      marginLeft: theme.spacing.unit,
-      marginRight: theme.spacing.unit,
-      width: 200,
-    },
-    menu: {
-      width: 200,
-    },
-  });      
+  container: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200
+  },
+  menu: {
+    width: 200
+  }
+});
 
 class Form extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            name: '',
-            firstname: '',
-            mail: '',
-            phoneNumber: '',
-            msg: '',
-            birthday: '',
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      firstname: "",
+      mail: "",
+      phoneNumber: "",
+      msg: "",
+      birthday: ""
+    };
+  }
 
-    handleChange = name => event => {
-        this.setState({
-          [name]: event.target.value,
-        });
-      };
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value
+    });
+  };
 
-      handleDateChange = (birthday) => {
-        this.setState({ birthday });
-      }
+  handleDateChange = birthday => {
+    this.setState({ birthday });
+  };
 
-    render() {
-        const {classes}  = this.props;
+  onSubmit = () => {
+    console.log(this.state);
+  };
 
+  render() {
+    const { classes } = this.props;
 
     return (
-        <Card>
-            <CardHeader
-            title="Formulaire de contact"
-            subheader="Tous les champs doivent être valide pour envoyer un message"
-          />
+      <Card>
+        <CardHeader
+          title="Formulaire de contact"
+          subheader="Tous les champs doivent être valide pour envoyer un message"
+        />
         <CardContent>
-        <form className={classes.container} noValidate autoComplete="off">
-        <DatePicker
-            keyboard
-            clearable
-            className={classes.textField}
-            // value={selectedDate}
-            onChange={this.handleDateChange}
-            animateYearScrolling={false}
-          />
-        <TextField
-          required
-          id="name"
-          label="Nom"
-          className={classes.textField}
-          value={this.state.name}
-          onChange={this.handleChange('name')}
-          margin="normal"
-        />
-        <TextField
-          required
-          id="firstname"
-          label="Prénom"
-          className={classes.textField}
-          value={this.state.firstname}
-          onChange={this.handleChange('firstname')}
-          margin="normal"
-        />
-        <TextField
-          required
-          id="mail"
-          label="Mél"
-          value={this.state.firstname}
-          onChange={this.handleChange('mail')}
-          className={classes.textField}
-          margin="normal"
-        />
-        <TextField
-          required
-          id="phoneNumber"
-          label="Numéro de téléphone"
-          value={this.state.firstname}
-          className={classes.textField}
-          margin="normal"
-        />
-        <TextField
-          required
-          id="msg"
-          label="Message"
-          value={this.state.firstname}
-          className={classes.textField}
-          margin="normal"
-        />
-      </form>   
+          <form className={classes.container} noValidate autoComplete="off">
+            <DatePicker
+              keyboard
+              clearable
+              className={classes.textField}
+              // value={selectedDate}
+              onChange={this.handleDateChange}
+              animateYearScrolling={false}
+            />
+            <TextField
+              required
+              id="name"
+              label="Nom"
+              className={classes.textField}
+              value={this.state.name}
+              onChange={this.handleChange("name")}
+              margin="normal"
+            />
+            <TextField
+              required
+              id="firstname"
+              label="Prénom"
+              className={classes.textField}
+              value={this.state.firstname}
+              onChange={this.handleChange("firstname")}
+              margin="normal"
+            />
+            <TextField
+              required
+              id="mail"
+              label="Mél"
+              value={this.state.mail}
+              onChange={this.handleChange("mail")}
+              className={classes.textField}
+              margin="normal"
+            />
+            <TextField
+              required
+              id="phoneNumber"
+              label="Numéro de téléphone"
+              value={this.state.phoneNumber}
+              onChange={this.handleChange("phonenumber")}
+              className={classes.textField}
+              margin="normal"
+            />
+            <TextField
+              required
+              id="msg"
+              label="Message"
+              value={this.state.msg}
+              onChange={this.handleChange("msg")}
+              className={classes.textField}
+              margin="normal"
+            />
+          </form>
         </CardContent>
         <CardActions>
-          <Button raised color="primary">
+          <Button onClick={this.onSubmit} raised color="primary">
             Envoyer
           </Button>
         </CardActions>
-        </Card>
-    )}
-
+      </Card>
+    );
+  }
 }
 
-
 Form.propTypes = {
-    classes: PropTypes.object.isRequired,
-  };
-  
+  classes: PropTypes.object.isRequired
+};
 
 export default withStyles(styles)(Form);
